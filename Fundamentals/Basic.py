2c90c073b98e406f4952654e4846484f52521254
Prachi Ghadge <prachi.swe@gmail.com>
2021-06-23 20:19:59 -0700

    Fixing formatting issues
    Fixing formatting issues

--- Fundamentals/Basic.py
+++ Fundamentals/Basic.py
@@ -1,7 +1,7 @@
 #DATA STRUCTURES
 
 #Number : INT etc
-print(int(-1/2)) => 0
+print(int(-1/2)) 
 
 #Boolean : not, ^ [(a and not b) or (not a and b)], and , or
 if True or False and False : 
@@ -13,11 +13,11 @@ print(string)
 
 #List : Mutable
 list = [1,"Thats a list", 2]
-print(list);
+print(list)
 list.append(4)
 print(list[1])
-print(list[0:1]) => [1]
-print(len(list)) => 3
+print(list[0:1]) 
+print(len(list)) 
 
 #Tuple : Immutable
 tuple = (1,"Thats a list", 2)
@@ -25,36 +25,42 @@ print(tuple)
 
 #Set : Ordered Unique ELements
 list = [1,2,4,3,2,3]
-set(list) => [1,2,3,4]
+print(set(list))
 
 #LOOPS
 #while loop
-i =1
-while i<10 :
-print(i)
-i += 1
+i = 1
+while i < 10 :
+   print(i)
+   i += 1
 
 #for loop
-for i in range(0,10)
-print(i)
+for i in range(0, 10) :
+   print(i)
 
-for i in range(0,10,2):
-print(i)
+for i in range(0, 10, 2):
+   print(i)
+   
+for i in range(0, 10, -2):
+   print(i)
 
+#advanced for loop
 s = "Hello"
 for i in s:
-print(i)
+   print(i)
 
-l=[(1,2),(3,4)]
+l = [(1,2),(3,4)]
 for i,j in l:
-print(i,j)
+   print(i,j)
 
 #FUNCTIONS
 def factorial(k):
-  if(k>0):
-    if(k==1): return 1
+  if (k > 0):
+    if (k == 1): 
+         return 1
     result = k * factorial(k-1)
-    print(result)
   else :
     result = 0
   return result
+  
+print(factorial(1))
